c5b6599339a8dbae7dc063df61561280119b3837
batusekerci <69806635+batusekerci@users.noreply.github.com>
2020-08-17 18:09:19 +0300

    upload

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+export const routes: Routes = [
+  {path: 'home' , component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'dishdetail/:id', component: DishdetailComponent},
+  {path: 'contactus' , component: ContactComponent},
+  {path: 'aboutus' , component: AboutComponent},
+  {path: '' , redirectTo: '/home' , pathMatch: 'full'}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
